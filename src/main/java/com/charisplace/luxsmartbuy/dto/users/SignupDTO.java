9f2f2a1cbf70089db717308f53bb81e83c72748d
Realwale <olawaleagboola41@gmail.com>
2023-05-27 19:50:50 +0100

    SignupDTO created

--- src/main/java/com/charisplace/luxsmartbuy/dto/users/SignupDTO.java
+++ src/main/java/com/charisplace/luxsmartbuy/dto/users/SignupDTO.java
@@ -1,4 +1,17 @@
 package com.charisplace.luxsmartbuy.dto.users;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
 }
