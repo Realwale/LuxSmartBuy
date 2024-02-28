045878b9e4fa0af477052846f47d2b6df8a57a4e
Realwale <olawaleagboola41@gmail.com>
2024-02-28 23:53:41 +0100

    upgrade SwaggerConfig to version 2.0.2

--- src/main/java/com/charisplace/luxsmartbuy/config/SwaggerConfig.java
+++ src/main/java/com/charisplace/luxsmartbuy/config/SwaggerConfig.java
@@ -1,36 +1,36 @@
-//package com.charisplace.luxsmartbuy.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public Docket productApi(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(getApiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.charisplace.luxsmartbuy"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//
-//    private ApiInfo getApiInfo(){
-//        Contact contact = new Contact("luxsmartbuy", "http://luxsmartbuy.com", "contact.luxsmartbuy@gmail.com");
-//        return new ApiInfoBuilder()
-//                .title("LuxSmartBuy API")
-//                .description("Documentation LuxSmartBuy API")
-//                .version("1.0.0")
-//                .license("http://www.apache.org/licences/LICENSE-2.0")
-//                .contact(contact)
-//                .build();
-//    }
-//}
+package com.charisplace.luxsmartbuy.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                description = "API documentation for LuxSmartBuy E-commerce Web Application",
+                title = "LuxSmartBuy Online Store Application API",
+                contact = @Contact(name = "Olawale Agboola",
+                        email = "agboolawale8@gmail.com",
+                        url = "https://www.github.com/Realwale"),
+                version = "1.0",
+                license = @License(name = "Apache License", url = "https://www.apache.org/licenses/LICENSE-2"),
+                termsOfService = "Terms of Service"
+        ),
+        security = {
+                @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = "Bearer Authentication", scopes = { "global" }),
+        }
+)
+
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+        name = "Bearer Authentication",
+        description = "JWT Authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class SwaggerConfig {
+}
